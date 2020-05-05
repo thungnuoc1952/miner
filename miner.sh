73e4ed8279277bca28d7801dc604771f56cfc6ae
thungnuoc1952 <64837706+thungnuoc1952@users.noreply.github.com>
2020-05-05 18:50:34 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -2,6 +2,7 @@ sudo yum install git automake gcc make curl-devel screen -y
 sudo yum install openssl-devel -y
 sudo yum install gcc-c++.x86_64 -y
 git clone https://github.com/tpruvot/cpuminer-multi.git
+sudo -i
 cd cpuminer-multi/
 ./autogen.sh
 ./configure --with-crypto --with-curl
