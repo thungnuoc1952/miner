5aa71f3258ae64b86a44230c4227cb69a4cbf870
thungnuoc1952 <64837706+thungnuoc1952@users.noreply.github.com>
2020-05-05 16:53:58 +0700

    miner.sh
    miner.sh

---
+++ miner.sh
@@ -0,0 +1,10 @@
+sudo yum install git automake gcc make curl-devel -y
+sudo yum install openssl-devel -y
+sudo yum install gcc-c++.x86_64 -y
+git clone https://github.com/tpruvot/cpuminer-multi.git
+cd cpuminer-multi
+./autogen.sh
+./configure --with-crypto --with-curl
+make
+make install
+./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u duc1951996@gmail.com -p x
